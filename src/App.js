3bf72ff609baefe7804275ea4dd1038008507f40
quentinsd <quentin_51@live.fr>
2021-04-13 18:39:38 +0200

    create Home.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import React, { useState } from "react";
+import Home from "./Home";
 
 function App() {
-  return <div className="App"></div>;
+  const [page, setPage] = useState(1);
+  return (
+    <div className="App">
+      {page === 1 && <Home page={setPage} />}
+      {page === 2 && <p>ok</p>}
+    </div>
+  );
 }
 
 export default App;
