d53dcec0836af24a0e2abf2ad6f8b86edc5839a7
quentinsd <quentin_51@live.fr>
2021-04-14 18:43:56 +0200

    add text and style

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,9 @@
 import React from "react";
+import Typical from "react-typical";
 
 function Home() {
   return (
-    <div>
+    <div className="w-screen h-screen flex">
       <video
         id="videoBG"
         autoplay="autoplay"
@@ -10,7 +11,7 @@ function Home() {
         loop="loop"
         className="fixed left-1/2 top-1/2 min-h-full min-w-full w-screen h-screen transform -translate-x-1/2 -translate-y-1/2 object-cover"
       >
-        <source src="./video/portfolio2.mp4" type="video/mp4" />
+        <source src="./video/portfolio3.mp4" type="video/mp4" />
       </video>
       <svg x="0" y="0" width="100vw" height="100vh" className="absolute z-0">
         <defs>
@@ -34,6 +35,27 @@ function Home() {
           fill="url(#diagonalHatch)"
         />
       </svg>
+      <div className="flex flex-col justify-center items-center w-screen h-sreen">
+        <p
+          className="text-center text-white relative text-4xl"
+          style={{ textShadow: "2px 2px 18px #000000" }}
+        >
+          Vous recherchez
+        </p>
+        <Typical
+          steps={[
+            "un développeur 💻",
+            1500,
+            "un collaborateur 🤝",
+            1500,
+            "un pationné 🎮",
+            1500,
+          ]}
+          loop={Infinity}
+          wrapper="p"
+          className="text-center text-yellow-400 relative text-4xl"
+        />
+      </div>
     </div>
   );
 }
